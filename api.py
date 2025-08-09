86e835afd9dbd3b76ee2309232745d92e8b5fae4
Umair Ali <mrumairali444@gmail.com>
2025-08-10 00:18:17 +0500

    styling for latpos working

---
+++ api.py
@@ -0,0 +1,20 @@
+# api.py
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+model = joblib.load('heart_disease_model.pkl')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    features = np.array([list(data.values())]).astype(float)
+    prediction = model.predict(features)[0]
+    return jsonify({'prediction': int(prediction)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
